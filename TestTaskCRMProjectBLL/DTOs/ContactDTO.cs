3e324aac74e627d31226ae9adc3351746bba797f
Igor <Igor@DESKTOP-C9JT6FV>
2022-09-13 16:21:16 +0300

    feat:  create DTOs,services,interfaces.add AutoMapper to project

---
+++ TestTaskCRMProjectBLL/DTOs/ContactDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTaskCRMProjectBLL.DTOs
+{
+    public class ContactDTO
+    {
+        public string Name { get; set; }
+        public int MobilePhone { get; set; }
+        public string JobTitle { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
---
+++ TestTaskCRMProjectBLL/Services/ContactService.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestTaskCRMProjectBLL.DTOs;
+using TestTaskCRMProjectBLL.Interfaces;
+using TestTaskCRMProjectDAL.Interfaces;
+using TestTaskCRMProjectDAL.Models.Entities;
+
+namespace TestTaskCRMProjectBLL.Services
+{
+    public class ContactService : IContactService
+
+    {
+        private IMapper _mapper;
+        private IContactRepository _contactRepository;
+        public ContactService(
+            IContactRepository contactRepository,
+            IMapper mapper)
+        {
+            _mapper = mapper;
+            _contactRepository = contactRepository;
+        }
+        public async Task<IEnumerable<ContactDTO>> Get()
+        {
+            var contacts = await _contactRepository.Get();
+            var contactDTOs = _mapper.Map<IEnumerable<ContactDTO>>(contacts);
+            return contactDTOs;
+        }
+        public async Task<ContactDTO> GetItem(int id)
+        {
+            var contact = await _contactRepository.GetItem(id);
+            var contactDTO = _mapper.Map<ContactDTO>(contact);
+            return contactDTO;
+        }
+        public async Task Edit(EditContactDTO editContactDTO)
+        {
+            var contact = _mapper.Map<Contact>(editContactDTO);
+            await _contactRepository.Edit(contact);
+        }
+        public async Task Delete(int id)
+        {
+            await _contactRepository.Delete(id);
+        }
+        public async Task Create(CreateContactDTO createContactDTO)
+        {
+            var contact = _mapper.Map<Contact>(createContactDTO);
+            await _contactRepository.Create(contact);
+        }
+
+    }
+}
